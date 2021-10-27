084e88645386cd3ea6e37bdb248a4c6ca2e6d224
haclamthien <haclamthien@gmail.com>
2021-10-27 10:21:37 +0700

    aaaaa

--- app/src/main/java/com/ctk43/doancoso/MainActivity.java
+++ app/src/main/java/com/ctk43/doancoso/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton btn_add_new_job;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        btn_add_new_job = findViewById(R.id.add_new_job);
+        btn_add_new_job.setBackgroundTintMode(null);
     }
 }
